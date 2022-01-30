e4a1b78af5bf6afac3e7a5fdb4bab885d3204f99
Ilya Shpigor <petrsum@gmail.com>
2022-01-30 13:17:52 +0100

    Refactoring of the target object

--- view.py
+++ view.py
@@ -2,7 +2,10 @@ import pygame
 import sys
 import random
 import math
+
+from dataclasses import dataclass
 from pygame.locals import *
+
 
 _COLOR_GREY = (128, 128, 128)
 _COLOR_BLUE = (85, 206, 255)
@@ -12,9 +15,19 @@ _WINDOW_WIDTH = 750
 _WINDOW_HEIGHT = 750
 _FPS = 75
 
+FONT = None
+
+
+KEY_INDEX = 0
 _KEY_COMBO = "weq"
 
-FONT = None
+
+@dataclass
+class Target:
+    x: int
+    y: int
+    size: int
+
 
 def terminate():
     pygame.quit()
@@ -39,24 +52,25 @@ def make_window(title):
 
 
 def draw_target(window_surface):
-  target_x = random.randint(20, _WINDOW_WIDTH - 20)
-  target_y = random.randint(20, _WINDOW_HEIGHT - 20)
-  target_size = random.randint(14, 20)
+  target = Target(
+    x = random.randint(20, _WINDOW_WIDTH - 20), \
+    y = random.randint(20, _WINDOW_HEIGHT - 20), \
+    size = random.randint(14, 20))
 
   pygame.draw.circle(window_surface, _COLOR_BLUE, \
-                     (target_x, target_y), target_size)
+                     (target.x, target.y), target.size)
 
-  return target_x, target_y, target_size
+  return target
 
 
-def is_mouse_on_target(target_x, target_y, target_size):
+def is_mouse_on_target(target):
   mouse_x = pygame.mouse.get_pos()[0]
   mouse_y = pygame.mouse.get_pos()[1]
 
-  sqx = (mouse_x - target_x)**2
-  sqy = (mouse_y - target_y)**2
+  sqx = (mouse_x - target.x)**2
+  sqy = (mouse_y - target.y)**2
 
-  return math.sqrt(sqx + sqy) < target_size
+  return math.sqrt(sqx + sqy) < target.size
 
 
 def draw_text(window_surface, text, x, y):
@@ -67,7 +81,6 @@ def draw_text(window_surface, text, x, y):
   text_rectangle.topleft = (x, y)
   window_surface.blit(text_object, text_rectangle)
 
-KEY_INDEX = 0
 
 def is_combo_pressed(key):
   global KEY_INDEX
@@ -89,7 +102,7 @@ def is_combo_pressed(key):
 
 def main_loop(window_surface, clock):
   draw_text(window_surface, _KEY_COMBO, 20, 20)
-  target_x, target_y, target_size = draw_target(window_surface)
+  target = draw_target(window_surface)
 
   while True:
 
@@ -98,11 +111,11 @@ def main_loop(window_surface, clock):
         terminate()
       if event.type == KEYDOWN:
         if is_combo_pressed(event.key) \
-           and is_mouse_on_target(target_x, target_y, target_size):
+           and is_mouse_on_target(target):
 
           window_surface.fill(_COLOR_GREY)
           draw_text(window_surface, "weq", 20, 20)
-          target_x, target_y, target_size = draw_target(window_surface)
+          target = draw_target(window_surface)
 
     pygame.display.update()
     clock.tick(_FPS)

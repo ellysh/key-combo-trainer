234bcaa25b77d4af072f7dca8dab2894100d7060
Ilya Shpigor <petrsum@gmail.com>
2022-01-30 19:12:45 +0100

    Generate the combo keys randomly

--- model.py
+++ model.py
@@ -1,17 +1,37 @@
+import random
+
+
+_KEY_SYMBOLS = "123aqwerdf"
+_KEY_LENGTH = 3
+
+
 KEY_INDEX = 0
-_KEY_COMBO = "weq"
+KEY_COMBO = ""
+#_KEY_COMBO = "weq"
+
+
+def generate_key_combo(symbols, length):
+  return ''.join(random.choices(symbols, \
+                 k = random.randrange(1, length)))
+
+
+def update_key_combo():
+  global KEY_COMBO
+
+  KEY_COMBO = generate_key_combo(_KEY_SYMBOLS, _KEY_LENGTH)
+
 
 def is_combo_pressed(key, target, is_mouse_on_target):
   global KEY_INDEX
 
-  if key == ord(_KEY_COMBO[KEY_INDEX]) \
+  if key == ord(KEY_COMBO[KEY_INDEX]) \
      and is_mouse_on_target(target):
 
-    if KEY_INDEX+1 == len(_KEY_COMBO):
+    if KEY_INDEX+1 == len(KEY_COMBO):
       KEY_INDEX = 0
       return True
 
-    elif KEY_INDEX+1 < len(_KEY_COMBO):
+    elif KEY_INDEX+1 < len(KEY_COMBO):
       KEY_INDEX += 1
       return False
 

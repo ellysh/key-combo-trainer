53747ad5e365bbccb44a29e51034b9e413a514ca
Ilya Shpigor <petrsum@gmail.com>
2022-01-31 11:56:32 +0100

    Print the version number in the window title

--- lol-combo-trainer.py
+++ lol-combo-trainer.py
@@ -3,8 +3,10 @@
 from view import make_window
 from view import main_loop
 
+_VERSION = "0.1.0"
+
 def main():
-  window_surface, clock = make_window("League of Legends Combos Trainer")
+  window_surface, clock = make_window("League of Legends Combos Trainer " + _VERSION)
 
   main_loop(window_surface, clock)
 
